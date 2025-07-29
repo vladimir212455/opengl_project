6d2bc51876ecc5daa98e6cf4f9e4608d78ad12a6
vladimir212455 <vladimirmaznyi08@gmail.com>
2025-07-29 17:12:23 +0700

    sec commit

--- src/main.cpp
+++ src/main.cpp
@@ -154,6 +154,8 @@ int main()
     {
         processInput(window);
         
+        deltaTime = glfwGetTime();
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
